713ee95c5b155b0f266c18d2c00bb6c69b0d759b
yeji0407 <yejisoft@gmail.com>
2021-04-29 16:31:45 +0900

    add /.travis.yml /travis/push.sh

---
+++ travis/push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "yejisoft@gmail.com"
+  git config --global user.name "Hong Yeji"
+}
+
+commit_deb_files() {
+  git checkout -b deploy
+  mkdir packages && mv ../*.deb packages/
+  git add packages/*
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add new https://${GH_TOKEN}@github.com/yeji0407/systemback.git > /dev/null 2>&1
+  git push new deploy
+}
+
+setup_git
+commit_deb_files
+upload_files
